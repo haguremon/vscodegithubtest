d9703d56c6a9990411225b94a2c16d48479df679
Yuta iwasaki <yanyong0127@gmail.com>
2022-02-11 12:31:48 +0900

    'test'

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+//
 void main() => {
       runApp(
+        //MaterialAppが下のものを持ってる
         const MaterialApp(
           home: Center(
             child: Text('git hub test'),
