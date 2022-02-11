4d02b404bf89979e32f5e56f5da523672f2e4ee6
Yuta iwasaki <yanyong0127@gmail.com>
2022-02-11 15:40:24 +0900

    'Scaffold()の構成、yamlファイルを弄ってimageを追加などをした'

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 //the main fnuction is the starting point for all our flutter
-void main() => {
-      runApp(
-        //実行するアプリはMaterialAppでCenter、widgetがありchildにtext；widgetを配置してる
-        const MaterialApp(
-          home: Center(
-            child: Text('git hub test'),
-          ),
-        ),
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 31, 74, 33),
+        appBar: AppBar(
+          centerTitle: true, title: Text('I_am_Rich'),
+          backgroundColor: Color.fromARGB(255, 46, 126, 49),),
+        body: Center(
+          child: Image(
+            //yamlファイルを読み込む
+            image: AssetImage('images/宝石.jpeg')))
       ),
-    };
+    ),
+  );
+}
